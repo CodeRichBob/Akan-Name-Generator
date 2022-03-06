43d6ff938ce159508c1f2539215f4f3371b85900
Robert <rgitonga.07@gmail.com>
2022-03-06 23:26:50 +0300

    declaring variables and assigning value according to user input

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 const boyNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 const girlNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+function findName (){
+    var yearOfBirth = document.getElementById("year").value
+    var monthOfBirth = document.getElementById("month").value
+    var dateOfBirth = document.getElementById("day").value 
+}
