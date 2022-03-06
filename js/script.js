3c7c5b87a102f07630b20a067938d78c7e30230b
Robert <rgitonga.07@gmail.com>
2022-03-07 00:08:15 +0300

    validating user input

--- js/script.js
+++ js/script.js
@@ -12,5 +12,13 @@ function findName (){
     var cc = math.tranc(yy/100);
 
     //calculating day of the week
-    var dayOfTheWeek = (((cc/4) - 2(cc-1) + ((5*yy/4)) + ((26*(mm+1)/10))+dd)%7
+    var dayOfTheWeek = (((cc/4) - 2(cc-1) + ((5*yy/4)) + ((26*(mm+1)/10))+dd)%7;
+    // adding some validation
+    if (yy<1000 || yy > 2022){
+        alert("Please enter a valid Year of Birth");
+    }else if (mm<=0 || mm > 12 || mm == 2 && dd > 29){
+        alert("Please Enter a valid Month of birth");
+    }else if (dd < 0 || dd > 31){
+        alert("Please enter a valid Date of Birth");
+    }
 }
