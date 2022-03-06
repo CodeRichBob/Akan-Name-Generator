36f01821e35258877ad100c5c3d0e83c98d31ffa
Robert <rgitonga.07@gmail.com>
2022-03-06 23:47:58 +0300

    parse user input values to integers

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-const boyNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-const girlNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+const boyNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+const girlNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 function findName (){
-    var yearOfBirth = document.getElementById("year").value
-    var monthOfBirth = document.getElementById("month").value
-    var dateOfBirth = document.getElementById("day").value 
+    //declaring variables and assigning them value from the user's input
+    var yearOfBirth = document.getElementById("year").value;
+    var monthOfBirth = document.getElementById("month").value;
+    var dateOfBirth = document.getElementById("day").value; 
+    //parsing user input to integers
+    var yy = parseInt(yearOfBirth);
+    var mm = parseInt(monthOfBirth);
+    var dd = parseInt(dateOfBirth);
+    var cc = math.tranc(yy/100);
+
+    //calculating day of the week
+    var dayOfTheWeek = 
 }
