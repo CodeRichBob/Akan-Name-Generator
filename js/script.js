fd048b6e72d704c069ed7fbc86d9b191e53e0291
Robert <rgitonga.07@gmail.com>
2022-03-07 03:14:09 +0300

    st: adding images and styling home page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const boyNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-const girlNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-const dayOfBirth = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+var boyNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var girlNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var dayOfBirth = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 function findName (){
     //declaring variables and assigning them value from the user's input
     var yearOfBirth = document.getElementById("year").value;
@@ -8,19 +8,20 @@ function findName (){
     var dateOfBirth = document.getElementById("day").value; 
     var gender = document.getElementByName("gender").value
     //parsing user input to integers
-    var yy = parseInt(yearOfBirth);
+    var yearr = parseInt(yearOfBirth);
+    var yy = parseInt(yearOfBirth.substr(2));
     var mm = parseInt(monthOfBirth);
     var dd = parseInt(dateOfBirth);
-    var cc = math.tranc(yy/100);
+    var cc = parseInt(yearOfBirth.substr(0,2));
 
     //calculating day of the week
-    var dayOfTheWeek = (((cc/4) - 2(cc-1) + ((5*yy/4)) + ((26*(mm+1)/10))+dd)%7;
+    var dayOfTheWeek = Math.abs(parseInt(((cc/4) - 2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10))+dd)%7);
     // adding some validation
-    if (yy<1000 || yy > 2022){
+    if(yearr<1000 || yearr > 2022){
         alert("Please enter a valid Year of Birth");
     }else if (mm<=0 || mm > 12 || mm == 2 && dd > 29){
         alert("Please Enter a valid Month of birth");
-    }else if (dd < 0 || dd > 31){
+    }else if (dd <= 0 || dd > 31){
         alert("Please enter a valid Date of Birth");
     }
     //checking name based on day of birth
